d341932791f05725df8961785913f2cde05cc89f
cesar <cesar@CESARB>
2026-02-17 23:55:11 -0600

    Crud4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,11 +39,12 @@ $routes->get('usuarios/eliminar/(:num)', 'UsuarioController::eliminarVista/$1');
 |--------------------------------------------------------------------------
 */
 $routes->get('api/usuarios', 'UsuarioController::listar');
+$routes->get('api/usuarios/buscar', 'UsuarioController::buscar');
 $routes->get('api/usuarios/(:num)', 'UsuarioController::obtener/$1');
 $routes->post('api/usuarios', 'UsuarioController::guardar');
 $routes->post('api/usuarios/(:num)', 'UsuarioController::actualizar/$1');
 $routes->delete('api/usuarios/(:num)', 'UsuarioController::eliminar/$1');
-$routes->get('api/usuarios/buscar', 'UsuarioController::buscar');
+
 
 
 
